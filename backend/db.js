d950fb9ac420b6fc671eb1cb9e0513ab9a29349c
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-10-06 00:54:08 +0530

    database name updated as instagram..

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'Instagram',
+  database: 'instagram',
   password: 'Mountblue@17',
   port: 5432,
 });
