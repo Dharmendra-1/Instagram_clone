f86bd4709486c7e3880ca39cd00b715870b27d23
Nikhil <nikhil.chopra.17@mountblue.tech>
2021-10-07 10:31:42 +0530

    add css to signup

--- frontend/src/components/screens/Signup.js
+++ frontend/src/components/screens/Signup.js
@@ -32,72 +32,75 @@ class Signup extends React.Component {
 
   render() {
     return (
-      <div className='row'>
-        <form onSubmit={this.createUser.bind(this)} className='col s12'>
-          <div className='row'>
-            <div className='input-field col s6'>
-              <input
-                placeholder='First Name'
-                name='firstName'
-                id='firstName'
-                type='text'
-                value={this.state.firstName}
-                onChange={this.handleChange.bind(this)}
-                className='validate'
-              />
+      <div className='mycard'>
+        <div className='row card auth-card'>
+          <h2>Instagram</h2>
+          <form onSubmit={this.createUser.bind(this)} className='col s12'>
+            <div className='row'>
+              <div className='input-field col s6'>
+                <input
+                  placeholder='First Name'
+                  name='firstName'
+                  id='firstName'
+                  type='text'
+                  value={this.state.firstName}
+                  onChange={this.handleChange.bind(this)}
+                  className='validate'
+                />
+              </div>
+              <div className='input-field col s6'>
+                <input
+                  placeholder='Last Name'
+                  id='lastName'
+                  name='lastName'
+                  type='text'
+                  value={this.state.lastName}
+                  onChange={this.handleChange.bind(this)}
+                  className='validate'
+                />
+              </div>
             </div>
-            <div className='input-field col s6'>
-              <input
-                placeholder='Last Name'
-                id='lastName'
-                name='lastName'
-                type='text'
-                value={this.state.lastName}
-                onChange={this.handleChange.bind(this)}
-                className='validate'
-              />
+            <div className='row'>
+              <div className='input-field col s12'>
+                <input
+                  placeholder='email@email.com'
+                  id='email'
+                  name='email'
+                  type='email'
+                  value={this.state.email}
+                  onChange={this.handleChange.bind(this)}
+                  className='validate'
+                />
+              </div>
             </div>
-          </div>
-          <div className='row'>
-            <div className='input-field col s12'>
-              <input
-                placeholder='email@email.com'
-                id='email'
-                name='email'
-                type='email'
-                value={this.state.email}
-                onChange={this.handleChange.bind(this)}
-                className='validate'
-              />
+            <div className='row'>
+              <div className='input-field col s12'>
+                <input
+                  placeholder='password'
+                  id='password'
+                  name='password'
+                  type='password'
+                  value={this.state.password}
+                  onChange={this.handleChange.bind(this)}
+                  className='validate'
+                />
+              </div>
             </div>
-          </div>
-          <div className='row'>
-            <div className='input-field col s12'>
-              <input
-                placeholder='password'
-                id='password'
-                name='password'
-                type='password'
-                value={this.state.password}
-                onChange={this.handleChange.bind(this)}
-                className='validate'
-              />
-            </div>
-          </div>
 
-          <div className='row'>
-            <div className='input-field col s12'>
-              <button
-                className='btn-block btn waves-effect waves-light #2196f3 blue'
-                type='submit'
-                name='action'
-              >
-                Submit
-                <i className='material-icons right'>send</i>
-              </button>
+            <div className='row'>
+              <div className='input-field col s12'>
+                <button
+                  className='btn-block btn waves-effect waves-light #2196f3 blue'
+                  type='submit'
+                  name='action'
+                >
+                  SignUp
+                  <i className='material-icons right'>send</i>
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     );
   }
