2e1d08aff7298a87686e9b4b521687c72b24c2d5
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-10-08 10:11:14 +0530

    for validation  email, name, password middleware created.

---
+++ backend/users/middleware/validInfo.js
@@ -0,0 +1,25 @@
+module.exports = function (req, res, next) {
+  const { firstName, lastName, email, password } = req.body;
+
+  function validEmail(userEmail) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail);
+  }
+
+  if (req.path === '/signup') {
+    console.log(!email.length);
+
+    if (![email, firstName, password].every(Boolean)) {
+      return res.status(401).json('Missing Credentials');
+    } else if (!validEmail(email)) {
+      return res.status(401).json('Invalid Email');
+    }
+  } else if (req.path === '/login') {
+    if (![email, password].every(Boolean)) {
+      return res.status(401).json('Missing Credentials');
+    } else if (!validEmail(email)) {
+      return res.status(401).json('Invalid Email');
+    }
+  }
+
+  next();
+};
