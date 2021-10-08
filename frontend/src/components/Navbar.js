37d23855cfcebefe58a03a52536ec73e5c7b8a2b
Subhadeep Bandyopadhyay <subhadeep.bandyopadhyay.17@mountblue.tech>
2021-10-08 14:04:25 +0530

    added logout button

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,21 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <nav>
-      <div className="nav-wrapper white">
-      <Link to="/"></Link>
-      <Link to="/signup"></Link>
-        <Link to="/" className="brand-logo left">
+      <div className='nav-wrapper white'>
+        <Link to='/'></Link>
+        <Link to='/signup'></Link>
+        <Link to='/' className='brand-logo left'>
           Instagram
         </Link>
-        <ul id="nav-mobile" className="right">
+        <ul id='nav-mobile' className='right'>
           <li>
-            <Link to="/home">Home</Link>
+            <Link to='/home'>Home</Link>
           </li>
           <li>
-            <Link to="/profile">Profile</Link>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          <li>
+            <Link
+              className='btn waves-effect waves-light #2196f3 blue logout'
+              to='/'
+              onClick={() => {
+                localStorage.clear();
+              }}
+            >
+              Log Out
+            </Link>
           </li>
         </ul>
       </div>
@@ -23,4 +34,4 @@ const NavBar = () => {
   );
 };
 
-export default NavBar;
+export default withRouter(NavBar);
