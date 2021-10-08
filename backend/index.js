5f98cd3410fd1f4ce38931679deb96319b9d1790
Dharmendra-1 <dharmendra.singh.17@mountblue.tech>
2021-10-08 10:14:24 +0530

    dashboard middleware added.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 4000;
-const userRoutes = require("../backend/users/routes");
-const cors = require("cors");
+const userRoutes = require('./users/routes/routes');
+const dashboard = require('./users/routes/dashboard');
+const cors = require('cors');
 
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use("/user", userRoutes);
+app.use('/user', userRoutes);
+app.use('/dashboard', dashboard);
 
 app.listen(port, () => {
   console.log(`Server Running on http://localhost:${port}`);
