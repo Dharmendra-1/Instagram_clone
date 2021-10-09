b6d110fb511f28bef590f19b45d0dbda922f7f69
Junaid Khan <junaid.khan.17@mountblue.tech>
2021-10-09 18:50:39 +0530

    connected with backend api

--- frontend/src/components/screens/CreatePost.js
+++ frontend/src/components/screens/CreatePost.js
@@ -1,7 +1,66 @@
 import React from 'react';
-// import M from 'materialize-css'
-// import { useHistory } from 'react-router-dom';
+
 class CreatePost extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+      body: '',
+      img: '',
+      id: '',
+    };
+  }
+
+  postDetails = async () => {
+    try {
+      const data = new FormData();
+      data.append('file', this.state.img);
+      data.append('upload_preset', 'insta-clone');
+      data.append('cloud_name', 'dg3xxjlfx');
+      const response = await fetch(
+        '	https://api.cloudinary.com/v1_1/dg3xxjlfx/image/upload',
+        {
+          method: 'POST',
+          body: data,
+        }
+      );
+
+      const fileData = await response.json();
+      console.log(fileData.url);
+
+      await fetch('http://localhost:4000/user/post', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: this.state.title,
+          body: this.state.body,
+          img: fileData.url,
+          id: this.state.id,
+        }),
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  getUserId = async () => {
+    const res = await fetch('http://localhost:4000/dashboard/', {
+      method: 'POST',
+      headers: { jwt_token: localStorage.token },
+    });
+    const parseData = await res.json();
+    console.log(parseData);
+    this.setState({ id: parseData.id });
+  };
+
+  componentDidMount() {
+    this.getUserId();
+  }
+
   render() {
     return (
       <div
@@ -16,19 +75,22 @@ class CreatePost extends React.Component {
         <input
           type='text'
           placeholder='title'
-          // value={title}
-          // onChange={(e)=>setTitle(e.target.value)}
+          value={this.state.title}
+          onChange={(e) => this.setState({ title: e.target.value })}
         />
         <input
           type='text'
           placeholder='body'
-          //  value={body}
-          // onChange={(e)=>setBody(e.target.value)}
+          value={this.state.body}
+          onChange={(e) => this.setState({ body: e.target.value })}
         />
         <div className='file-field input-field'>
           <div className='btn #64b5f6 blue darken-1'>
             <span>Uplaod Image</span>
-            {/* <input type="file" onChange={(e)=>setImage(e.target.files[0])} /> */}
+            <input
+              type='file'
+              onChange={(e) => this.setState({ img: e.target.files[0] })}
+            />
           </div>
           <div className='file-path-wrapper'>
             <input className='file-path validate' type='text' />
@@ -36,7 +98,7 @@ class CreatePost extends React.Component {
         </div>
         <button
           className='btn waves-effect waves-light #64b5f6 blue darken-1'
-          // onClick={()=>postDetails()}
+          onClick={() => this.postDetails()}
         >
           Submit post
         </button>
