8896f4f298b491b620c0ff2da9d3dfafd0645106
Nikhil <nikhil.chopra.17@mountblue.tech>
2021-10-09 15:52:19 +0530

    Home data map initiated

--- frontend/src/components/screens/Home.js
+++ frontend/src/components/screens/Home.js
@@ -2,65 +2,47 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: [],
+    };
+  }
   getUser = async () => {
     try {
       const res = await fetch('http://localhost:4000/user/home');
       const parseData = await res.json();
-      console.log(parseData);
+      this.setState({ userData: parseData });
     } catch (err) {
       console.error(err.message);
     }
   };
-
-  render() {
+  componentDidMount() {
     this.getUser();
+  }
+  render() {
     return (
       <div className='home'>
-        <div className='card home-card'>
-          <h5>jhon</h5>
-          <div className='card-image'>
-            <img
-              src='https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-              alt='post'
-            />
-          </div>
-          <div className='card-content'>
-            <i className='material-icons'>favorite_border</i>
-            <h6>title</h6>
-            <p>this is a post!</p>
-            <input type='text' placeholder='add a comment' />
-          </div>
-        </div>
-        <div className='card home-card'>
-          <h5>jhon</h5>
-          <div className='card-image'>
-            <img
-              src='https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-              alt='post'
-            />
-          </div>
-          <div className='card-content'>
-            <i className='material-icons'>favorite_border</i>
-            <h6>title</h6>
-            <p>this is a post!</p>
-            <input type='text' placeholder='add a comment' />
-          </div>
-        </div>
-        <div className='card home-card'>
-          <h5>jhon</h5>
-          <div className='card-image'>
-            <img
-              src='https://images.unsplash.com/photo-1508739773434-c26b3d09e071?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d2FsbHBhcGVyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
-              alt='post'
-            />
-          </div>
-          <div className='card-content'>
-            <i className='material-icons'>favorite_border</i>
-            <h6>title</h6>
-            <p>this is a post!</p>
-            <input type='text' placeholder='add a comment' />
-          </div>
-        </div>
+        {this.state.userData.map((data) => {
+          if (data.first_name) {
+            return (
+              <div key={data.id} className='card home-card'>
+                <h5>{data.profile + ' ' + data.first_name}</h5>
+                <div className='card-image'>
+                  <img src={data.image_url} alt='post' />
+                </div>
+                <div>
+                  <span>likes:</span>
+                  {data.like}
+                </div>
+                <div>{data.first_name + ' ' + data.caption}</div>
+                <div>{data.comments} </div>
+              </div>
+            );
+          } else {
+            return <div key={data.id}></div>;
+          }
+        })}
       </div>
     );
   }
