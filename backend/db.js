6a568d748b4c4ef01c521d31d51c217397bd9ceb
Junaid Khan <junaid.khan.17@mountblue.tech>
2021-10-15 20:23:37 +0530

    added database

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'instagram',
-  password: 'team1',
+  user: 'osftxcuczmsakg',
+  host: 'ec2-54-158-247-97.compute-1.amazonaws.com',
+  database: 'd4c0j60a6join9',
+  password: '24f31020f6cf036303aa02b8dafd14bdf06e994e28b98b33e95ce30c603d9196',
   port: 5432,
 });
 
